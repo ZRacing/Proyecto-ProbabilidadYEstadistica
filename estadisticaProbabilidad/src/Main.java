1db091a554ce854887e049a39b822dc02930f31b
Gabriel <gabriel.hdez.530@gmail.com>
2026-03-02 17:43:59 -0600

    Add files via upload

---
+++ estadisticaProbabilidad/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    Datos datos = new Datos();
+        datos.defArreglo();
+        datos.generarDatos();
+        datos.mostrarDatos();
+    }
+}
+
